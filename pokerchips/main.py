7b50fdf935066ee78fd7733bac6eb0b838329489
MarcelloFid <89382092+MarcelloFid@users.noreply.github.com>
2021-11-09 14:42:32 +0100

    v2

--- pokerchips/main.py
+++ pokerchips/main.py
@@ -7,7 +7,6 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-import math
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
@@ -20,6 +19,9 @@ ev3 = EV3Brick()
 ev3.speaker.beep()
 
 cd = ColorSensor(port=Port.S1)
+conveyer = Motor(Port.A)
+receptor = Motor(Port.B)
+
 
 color_values = {"red":(16,4,8),"white":(44,47,86),"blue":(4,8,24),"black":(4,4,6),"conveyer":(0,0,3)}
 
@@ -36,11 +38,27 @@ def ReadColor(coordinate):
             distance = new_distance 
     return return_color
 
-ev3.speaker.beep()
+color_to_box_position = {"red":1,"white":2,"blue":3,"black":4}
+pokerchip_count = {"red":0,"white":0,"blue":0,"black":0}
+value = {"red":10, "white":20, "blue":50, "black":100}
 
+previous_box_position = 2
+pot = 0
+conveyer.run(100)
 while True:
-    print(ReadColor(cd.rgb()))
-    wait(100)
+  
+    chip = ReadColor(cd.rgb())
+    if chip != "conveyer":
+        wait(200)
+        if chip == ReadColor(cd.rgb()):
+            print(chip)
+            pokerchip_count[chip] += 1
+            pot += value[chip]
+            box_shift = color_to_box_position[chip]-previous_box_position
+            receptor.run_target(100, receptor.angle()+30*box_shift)
+            previous_box_position = color_to_box_position[chip]
+            wait(1000)
+
 #Red 16 4 8
 #White 44 47 86
 #Blue 4 8 24
